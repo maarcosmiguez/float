490fb04a40632b08facf98b4cdb1013951a26831
marcosrubik <maarcoscasas@gmail.com>
2024-09-25 22:47:49 -0300

    hidden env

--- firebase/config.ts
+++ firebase/config.ts
@@ -16,12 +16,12 @@ import {
 
 // Web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MG_SENDER_ID,
-  appId: process.env.APP_ID,
+  // apiKey: process.env.API_KEY,
+  // authDomain: process.env.AUTH_DOMAIN,
+  // projectId: process.env.PROJECT_ID,
+  // storageBucket: process.env.STORAGE_BUCKET,
+  // messagingSenderId: process.env.MG_SENDER_ID,
+  // appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
