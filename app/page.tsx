5745a115ca50a0aa922e5e5602bde8db73622bd0
marcosrubik <maarcoscasas@gmail.com>
2024-12-09 10:01:26 -0300

    added metadata

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Somos from "components/ui/Features/Somos";
 import Hero from "components/ui/Hero";
 import { PlanesDeSuscripcion } from "components/ui/PlanesDeSuscripcion";
 import Footer from "components/ui/Footer";
+import Head from "next/head";
 
 export default () => {
   useEffect(() => {
@@ -58,38 +59,78 @@ export default () => {
   }, []);
 
   return (
-    <div className="snap-y snap-mandatory overflow-y-scroll scrollbar-hide min-h-screen">
-      <section
-        className="snap-start flex items-center min-h-[100vh] justify-center section visible"
-        id="home"
-      >
-        <Hero />
-      </section>
-      <section
-        className="snap-start flex md:hidden items-center min-h-[90vh] justify-center section mb-20"
-        id="planes"
-      >
-        <PlanesDeSuscripcion />
-      </section>
-      <section
-        className="snap-start flex items-center min-h-[90vh] justify-center section"
-        id="contenido"
-      >
-        <Contenido />
-      </section>
-      <section
-        className="snap-start flex items-center min-h-[90vh] justify-center section"
-        id="somos"
-      >
-        <Somos />
-      </section>
-      <section
-        className="snap-start items-center min-h-[90vh] justify-center section mb-20 hidden md:flex"
-        id="planes"
-      >
-        <PlanesDeSuscripcion />
-      </section>
-      <Footer />
-    </div>
+    <>
+      {" "}
+      <Head>
+        {/* Metaetiquetas básicas */}
+        <title>Dopamina | Contenidos y planes</title>
+        <meta
+          name="description"
+          content="Dopamina: Noticias y contenidos frescos sobre actualidad, cultura y más."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Open Graph */}
+        <meta property="og:title" content="Dopamina | Tu dosis de actualidad" />
+        <meta
+          property="og:description"
+          content="Descubre lo último en noticias, cultura y tendencias. Dopamina es tu fuente confiable de inspiración y actualidad."
+        />
+        <meta
+          property="og:image"
+          content="public/thumbnail.png"
+        />
+        <meta property="og:url" content="https://dopamina.uy/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Cards */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Dopamina | Tu dosis de actualidad"
+        />
+        <meta
+          name="twitter:description"
+          content="Descubre lo último en noticias, cultura y tendencias con Dopamina."
+        />
+        <meta
+          name="twitter:image"
+          content="public/thumbnail.png"
+        />
+      </Head>
+      <div className="snap-y snap-mandatory overflow-y-scroll scrollbar-hide min-h-screen">
+        <section
+          className="snap-start flex items-center min-h-[100vh] justify-center section visible"
+          id="home"
+        >
+          <Hero />
+        </section>
+        <section
+          className="snap-start flex md:hidden items-center min-h-[90vh] justify-center section mb-20"
+          id="planes"
+        >
+          <PlanesDeSuscripcion />
+        </section>
+        <section
+          className="snap-start flex items-center min-h-[90vh] justify-center section"
+          id="contenido"
+        >
+          <Contenido />
+        </section>
+        <section
+          className="snap-start flex items-center min-h-[90vh] justify-center section"
+          id="somos"
+        >
+          <Somos />
+        </section>
+        <section
+          className="snap-start items-center min-h-[90vh] justify-center section mb-20 hidden md:flex"
+          id="planes"
+        >
+          <PlanesDeSuscripcion />
+        </section>
+        <Footer />
+      </div>
+    </>
   );
 };
